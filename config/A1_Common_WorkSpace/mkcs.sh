19c82e3fab9e20f13f765ac5ce22a699699dd42f
chungyihung1988 <chungyihung1988@gmail.com>
2021-02-08 18:41:10 +0800

    Update mkcs script
    Update the sources path.

--- config/A1_Common_WorkSpace/mkcs.sh
+++ config/A1_Common_WorkSpace/mkcs.sh
@@ -2,9 +2,18 @@
 rm -rf cscope.*
 rm -rf tags
 
+if [ "$#" == 0 ]; then
+    echo 'Use case: mkcs.sh [androsys|kernel|efi|java|python|mk|default|]'
+    exit
+fi
+
+
 if [ "$1" == "androsys" ]; then
-    find "./external" -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" >> cscope.files
-    find "./system" -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" >> cscope.files
+    find "./external" -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.cc" >> cscope.files
+    find "./system" -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.cc" >> cscope.files
+    find "./bionic" -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.cc" >> cscope.files
+    find "./development/" -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.cc" >> cscope.files
+    find "./vendor/qcom/opensource/core-utils/" -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.cc" >> cscope.files
     cscope -R -b -k -q -i cscope.files
     ctags -R -L cscope.files --exclude=.git
 elif [ "$1" == "efi" ]; then
@@ -12,10 +21,24 @@ elif [ "$1" == "efi" ]; then
     cscope -R -b -k -q -i cscope.files
     ctags -R -L cscope.files --exclude=.git
 elif [ "$1" == "java" ]; then
-    find . -iname "*.java" -o -iname "*.cpp" -o -iname "*.c" -o -name > cscope.files
+    find . -iname "*.java" -o -iname "*.cpp" -o -iname "*.c" -o -iname "*.hpp" -o -iname "*.h" > cscope.files
     cscope -R -b -k -q -i cscope.files
     ctags -R -L cscope.files --exclude=.git
-else
+elif [ "$1" == "kernel" ]; then
+    echo "Parsing source code in kernel mode..."
+    find . -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \
+        -o -name "*.dtsi" -o -name "*.dts" -o -iname "*.S" > cscope.files
+    cscope -R -b -k -q -i cscope.files
+    ctags -R -L cscope.files --exclude=.git
+elif [ "$1" == "python" ]; then
+    find . -iname "*.py" -o -iname "*.cpp" -o -iname "*.c" -o -iname "*.hpp" -o -iname "*.h" > cscope.files
+    cscope -R -b -k -q -i cscope.files
+    ctags -R -L cscope.files --exclude=.git
+elif [ "$1" == "mk" ]; then
+    find . -iname "*.py" -o -iname "*.cpp" -o -iname "*.c" -o -iname "*.hpp" -o -iname "*.h" -o -iname "*.mk" -o -iname "makefile" -o -iname "*.sh"> cscope.files
+    cscope -R -b -k -q -i cscope.files
+    ctags -R -L cscope.files --exclude=.git
+elif [ "$1" == "default" ]; then
     find . -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" > cscope.files
     cscope -R -b -k -q -i cscope.files
     ctags -R -L cscope.files --exclude=.git
